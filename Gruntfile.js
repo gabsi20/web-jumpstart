1ce835a480d798313a57f9db6a42e1105a4e7aa1
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-04-26 11:20:04 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    browserify: {
+      dist: {
+        options: {
+          transform: [
+            ["babelify", { presets: ["es2015"] }]
+          ]
+        },
+        files: {
+          "./dist/script/index.js": ["./src/scripts/index.js"]
+        }
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          './dist/style/style.css': './src/styles/index.scss'
+        }
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src',
+        src: ['**/*.{html, php}'],
+        dest: 'dist/',
+      },
+    },
+    clean: ['./dist/**/*', './dest/**/*'],
+    imagemin: {
+      dynamic: {
+        options: { // Target options
+          optimizationLevel: 6,
+          max: 60
+        },
+        files: [{
+          expand: true,
+          cwd: 'src', // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
+          dest: 'dist/' // Destination path prefix
+        }]
+      }
+    },
+    watch: {
+      scripts: {
+        files: ["./src/scripts/**/*.js"],
+        tasks: ["browserify"]
+      },
+      css: {
+        files: ["./src/styles/**/*.scss"],
+        tasks: ["sass"]
+      },
+      html: {
+        files: ["./src/**/*.html", './src/**/*.php'],
+        tasks: ["copy"]
+      },
+      images: {
+        files: ["./src/**/*.{png,jpg,gif}"],
+        tasks: ["imagemin"]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  grunt.registerTask("default", ["watch"]);
+  grunt.registerTask("build", ["copy", "browserify", "sass", "imagemin"]);
+};
---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$(function() {
+  // code comes here
+});
